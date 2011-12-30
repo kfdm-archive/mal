67cd411cafe4ba6c3c4525926d93c368e1f481ee
Paul Traylor <kungfudiscomonkey@gmail.com>
2011-12-29 22:10:34 -0800

    Move duplicate code into query_and_format()

--- mal/cli.py
+++ mal/cli.py
@@ -1,46 +1,42 @@
 #!/usr/bin/env python
 
 from sys import argv
-from mal import Mal
+from mal import Mal, Anime, Manga
 
-ANIME_FORMAT = '{a.title:<50.50} [{a.id:>5}] ' +\
-    '({a.my_watched:>3}/{a.episodes:<3})'
-MANGA_FORMAT = '{m.title:<50.50} [{m.id:>5}] ' +\
-    '({m.my_read_volumes:>3}/{m.volumes:<3}|{m.my_read_chapters:>3}/{m.chapters:<3})'
+ANIME_FORMAT = '{i.title:<50.50} [{i.id:>5}] ' +\
+    '({i.my_watched:>3}/{i.episodes:<3})'
+MANGA_FORMAT = '{i.title:<50.50} [{i.id:>5}] ' +\
+    '({i.my_read_volumes:>3}/{i.volumes:<3}|{i.my_read_chapters:>3}/{i.chapters:<3})'
+SCORE_FORMAT = ' <{i.my_score}>'
+
 
 mal = Mal()
 
-SORTER = lambda item: item.title.lower()
+
+def query_and_format(method, filter, format):
+    SORTER = lambda item: item.title.lower()
+    for item in sorted(method(argv[1], filter=filter).values(), key=SORTER):
+        print format.format(i=item)
 
 
 def main():
     print 'Currently Watching'
     print '-' * 80
-    for anime in sorted(mal.anime_list(argv[1]).values(), key=SORTER):
-        if anime.my_status is anime.WATCHING:
-            print ANIME_FORMAT.format(a=anime)
+    query_and_format(mal.anime_list, Anime.WATCHING, ANIME_FORMAT)
     print
     print 'Currently Reading'
     print '-' * 80
-    for manga in sorted(mal.manga_list(argv[1]).values(), key=SORTER):
-        if manga.my_status is manga.READING:
-            print MANGA_FORMAT.format(m=manga)
+    query_and_format(mal.manga_list, Manga.READING, MANGA_FORMAT)
 
 
 def completed():
-    AFORMAT = ANIME_FORMAT + ' <{a.my_score}>'
-    MFORMAT = MANGA_FORMAT + ' <{m.my_score}>'
     print 'Completed Anime'
     print '-' * 80
-    for anime in sorted(mal.anime_list(argv[1]).values(), key=SORTER):
-        if anime.my_status is anime.COMPLETED:
-            print AFORMAT.format(a=anime)
+    query_and_format(mal.anime_list, Anime.COMPLETED, ANIME_FORMAT + SCORE_FORMAT)
     print
     print 'Completed Manga'
     print '-' * 80
-    for manga in sorted(mal.manga_list(argv[1]).values(), key=SORTER):
-        if manga.my_status is manga.COMPLETED:
-            print MFORMAT.format(m=manga)
+    query_and_format(mal.manga_list, Manga.COMPLETED, MANGA_FORMAT + SCORE_FORMAT)
 
 if __name__ == '__main__':
     main()
