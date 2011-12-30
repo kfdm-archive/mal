615d8ef4ca8258ddee8d597ee7a36960fcdbcf42
Paul Traylor <kungfudiscomonkey@gmail.com>
2011-12-29 21:08:49 -0800

    Convert tabs to spaces

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 
 try:
-	from setuptools import setup
+    from setuptools import setup
 except ImportError:
-	from distutils.core import setup
+    from distutils.core import setup
 
 setup(
-	name='mal',
-	description='MyAnimeList Library in Python',
-	author='Paul Traylor',
-	url='http://github.com/kfdm/mal/',
-	version='0.1',
-	packages=['mal'],
-	install_requires=['requests'],
-	# http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	classifiers = [
-		'Development Status :: 3 - Alpha',
-		'Natural Language :: English',
-		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python',
-		'Operating System :: OS Independent',
-	],
-	entry_points = {
-		'console_scripts':[
-			'mal = mal.cli:main'
-		]
-	}
+    name='mal',
+    description='MyAnimeList Library in Python',
+    author='Paul Traylor',
+    url='http://github.com/kfdm/mal/',
+    version='0.1',
+    packages=['mal'],
+    install_requires=['requests'],
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mal = mal.cli:main'
+        ]
+    }
 )
